767a7cf03f563b27075ea195ae7744b7571a6b32
West Lane <west@paperequator.com>
2018-06-09 16:34:50 -0400

    Register WiFi SSID on Admin Page

--- src/admin/admin.js
+++ src/admin/admin.js
@@ -3,6 +3,26 @@ window.page = (function() {
 
 
     var self = new LanternPage("admin");
+    
+
+    function validateForm() {
+
+        var $data = self.view.$data;
+        
+        $data.processing = true;
+        
+        console.log("[network] storing wifi credentials...");
+        self.view.$http.post(self.getBaseURI() + "/api/config/ssid", {
+            "ssid": $data.network_ssid,
+            "pass": $data.network_pass
+        }).then(function(response) {
+            completeSetup();
+        }, function(err) {
+            $data.warning = "Please check your wifi credentials and try again.";
+            $data.processing = false;
+        });
+    }
+
 
     //------------------------------------------------------------------------
     self.addData("types", [
@@ -12,6 +32,12 @@ window.page = (function() {
         {key: "r", slug: "route"},
         {key: "n", slug: "note"},
     ]);
+
+    self.addData("warning", "");
+    self.addData("connection", false);
+    self.addData("network_ssid", "");
+    self.addData("network_pass", "");
+    self.addData("processing", false);
 
     //------------------------------------------------------------------------
     self.addHelper("loadTestData", function() {
@@ -53,7 +79,14 @@ window.page = (function() {
         catch(e) {
             console.log("can't get document count for ", type, e);
         }
+    });    
+
+
+    self.addHelper("handleSubmit", function() {
+        self.view.$data.processing = true;
+        setTimeout(validateForm, 150);
     });
+
 
 
 
@@ -63,6 +96,13 @@ window.page = (function() {
         .then(function() {
             self.view.$data.types.forEach(function(type) {
                 self.stor.getManyByType(type.key);
+            });
+
+            self.view.$watch("connection", function(new_val, old_val) {
+                if (new_val === false) {
+                    self.view.$data.warning = false;
+                    self.view.$data.network_pass = "";
+                }
             });
         });
 
